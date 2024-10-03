63b3ebc30d70fd81ed58135783609e11173958b2
CharlMMZ <moyamarquezcarlos@gmail.com>
2024-10-03 12:06:31 +0200

    commit 03/10/2024

---
+++ EjFizzBuzz/fizzbuzz.js
@@ -0,0 +1,45 @@
+function fizzBuzz() {
+    
+    let cliente;
+    do {
+        cliente = parseInt(prompt("Dame el tamaño de una matriz NxN:",""));
+        
+    } while (isNaN(cliente));
+    let matriz = []
+    
+    for (let i = 0; i < cliente; i++) { 
+        matriz[i]=[];
+
+        for (let j = 0; j < cliente; j++) {
+            matriz[i][j] = Math.floor(Math.random() * (cliente * 2 ) + cliente);
+            
+        }
+        
+    }
+    //Número mayor de la matriz y mostrarlo
+    let mayor = 0;
+    for (let i = 0; i < matriz.length; i++) {
+
+        for (let j = 0; j < matriz[i].length; j++) {
+            if (matriz[i][j]>mayor) {
+                mayor=matriz[i][j];
+            }
+            
+        }
+        
+    }
+    console.log(mayor);
+
+    //Mostrar la matriz en filas y columas
+    for (let i = 0; i < matriz.length; i++) {
+
+        for (let j = 0; j < matriz[i].length; j++) {
+                console.log(matriz[i][j]+" ");
+
+        }
+        if (i=cliente) {
+            console.log("\n");
+        }
+        
+    }
+}
