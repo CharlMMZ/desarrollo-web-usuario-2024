3e6f15ce5257976f9d7f4290c9f771693384f642
CharlMMZ <moyamarquezcarlos@gmail.com>
2024-10-24 10:58:56 +0200

    DOM 24/10/2024

--- EjDOM/Ej12/ej12.js
+++ EjDOM/Ej12/ej12.js
@@ -1,3 +1,4 @@
+/*
 function izquierda() {
     let imagen = document.getElementsByTagName("img")[0];
 
@@ -19,5 +20,25 @@ function derecha() {
     if (posicion>=5) {
         let res = posicion+5;
         document.body.style.setProperty("left", res);
+    }
+}
+*/
+window.onload = function() {
+    let bottones = document.getElementsByTagName("button");
+    for (let botton of bottones) {
+        mover(this);
+        
+    }
+}
+
+function mover(boton) {
+    let imagen = document.getElementsByTagName("img")[0];
+    let estilos = window.getComputedStyle(imagen);
+    let posicion = estilos.getPropertyValue("left");
+
+    if (boton.textContent == "Izquierda") {
+        imagen.style.setProperty("left", posicion-5+"px");
+    }else{
+        imagen.style.setProperty("right", posicion+5+"px");
     }
 }
